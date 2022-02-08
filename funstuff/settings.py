7e33b2c8ef3f9a0ac6cf7b3b7cd1fa14fd95a493
Anshel Infotech <anshelinfotech@gmail.com>
2022-02-08 21:00:15 +0530

    Basic funstuffapp created with home page and urls done

--- funstuff/settings.py
+++ funstuff/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'funstuffapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'funstuff.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
